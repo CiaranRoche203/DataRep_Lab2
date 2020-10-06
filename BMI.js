b96af361cdd5a61acab385f5636dfc767970a846
ciaranroche203 <rocheciaran37@gmail.com>
2020-10-06 18:25:50 +0100

    Second Commit

--- BMI.js
+++ BMI.js
@@ -1,15 +1,20 @@
 class BMI {
+    //bmi class
+    //takes in 2 arguments weight and height
     constructor(height, weight) {
         this.height = height;
         this.weight = weight;
     }
+    //calculating the bmi of user method
     calculateBMI() {
-
+        //calculation
         let bmi = this.weight / (this.height ** 2);
         return bmi;
+        //returns bmi as output
     }
 }
-
+//parameters used to calculate this users bmi
 let MyBmi = new BMI(2, 90);
 let calculatedBMI = MyBmi.calculateBMI();
+//displaying output
 console.log(calculatedBMI);
--- cars.js
+++ cars.js
@@ -1,28 +1,31 @@
 class Vehicle{
+    //vehicle class taking 3 parameters
     constructor(make, model, year){
         this.make = make;
         this.model = model;
         this.year = year;
-
     }
+    //information of the car method
     Information(){
         console.log(`Make: ${this.make}, Model: ${this.model}, Year: ${this.year}`);
-
     }
 }
 //let myVehicle = new Vehicle('VW', 'golf', 2000);
 //let info = myVehicle.Information();
-
+// cars class that inherites from vehicles 
 class Cars extends Vehicle{
+    //takes the orighinal 3 parameters as well as a new one
     constructor(make, model, year, doors){
         super(make, model, year);
         this.doors = doors;
     }
+    //information method that displays the output
     Information(){
         super.Information();
         console.log(`Doors: ${this.doors}`);
     }
 }
-
+//taking the information that is to be displayed
 let myCar = new Cars('VW', 'Golf', 2009, 5);
+//calling the method
 myCar.Information();
